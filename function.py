817ed76faa6cdd787136019037fe0f77d8f87888
Luiz Felipe <luizfelipesmaciel@gmail.com>
2020-03-28 16:14:39 -0300

    Done some refactoring

--- function.py
+++ function.py
@@ -9,32 +9,37 @@ class Function:
     def __call__(self, x):
         return self.__func(x)
 
+    def __choose_mode(self, dx, mode):
+
+        if mode not in ['backward', 'forward', 'central']:
+            raise InvalidModeException('mode must be backward, forward or central')
+
+        f = self
+        dfdx = None
+        if mode == 'central':
+            dfdx = Function(lambda x: (f(x + dx) - f(x - dx)) / (2 * dx))
+        elif mode == 'backward':
+            dfdx = Function(lambda x: (f(x) - f(x - dx)) / dx)
+        elif mode == 'forward':
+            dfdx = Function(lambda x: (f(x + dx) - f(x)) / dx)
+
+        return dfdx
+
     def derivative_at(self, x, order=1, dx=10e-7, mode='central'):
         '''
-            Return the value of the derivative of order <order>
+            Returns the value of the derivative of order <order>
             at <x> with  <dx> difference using <mode> approaching
         '''
 
         if order <= 0:
             raise InvalidOrderException('Order must be > 0')
-        
-        if mode not in ['backward', 'forward', 'central']:
-            raise InvalidModeException('mode must be backward, forward or central')
 
-        return self.__derivative(x,order=order, dx=dx, mode=mode)
-    
-    def __derivative_at(self, _x, curr_order=1, order=1, dx=10.e-7, mode='central'):
+        return self.__derivative_at(x, order=order, dx=dx, mode=mode)
+
+    def __derivative_at(self, x, curr_order=1, order=1, dx=10e-7, mode='central'):
 
         if curr_order <= order:
-            f = self
-            if mode == 'central':
-                dfdx = Function(lambda x: (f(x + dx) - f(x - dx)) / (2 * dx))
-            elif mode == 'backward':
-                dfdx = Function(lambda x: (f(x) - f(x - dx)) / dx)
-            elif mode == 'forward':
-                dfdx = Function(lambda x: (f(x + dx) - f(x)) / dx)
-            
-            return dfdx.__derivative(_x, curr_order+1, order, dx, mode)
-        
-        return self(_x)
+            dfdx = self.__choose_mode(dx, mode)
+            return dfdx.__derivative_at(x, curr_order+1, order, dx, mode)
 
+        return self(x)
