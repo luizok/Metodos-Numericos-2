dac493b03e007521db67b2a1c78d609dbb2c02e6
Luiz Felipe <luizfelipesmaciel@gmail.com>
2020-04-09 21:20:50 -0300

    Implemented partition approaching

--- numerical_integration/newton_cotes_1234.py
+++ numerical_integration/newton_cotes_1234.py
@@ -2,7 +2,36 @@ from math import sin
 from generic_newton_cotes import generic_newton_cotes
 
 
-def newton_cotes(p, func, a, b, closed_interval=True):
+def newton_cotes(p, func, a, b, tol=10e-6, closed_interval=True):
+    '''
+        Aproximate <func> using a polynomial of degree <p>
+        to integrate [<a>, <b>] if <closed_interval> is true,
+        else (<a>, <b>) until reaches the absolute error < <tol>
+    '''
+    curr_value = float('inf')
+    prev_value = 0
+    n = 0
+
+    while(abs(curr_value - prev_value) > tol):
+        iters = 0
+        total_sum = 0
+        dx = (b - a) / 2**n
+
+        for i in range(2**n):
+            is_closed = not closed_interval if not closed_interval and 1 <= i < 2**n-1 else closed_interval
+            total_sum += newton_cotes_partition(p, func,
+                            a + i*dx, a + (i + 1)*dx,
+                            is_closed)
+
+        prev_value = curr_value
+        curr_value = total_sum
+
+        n += 1
+
+    return n, total_sum
+
+
+def newton_cotes_partition(p, func, a, b, closed_interval=True):
     '''
         Aproximate <func> using a polynomial of degree <p>
         to integrate [<a>, <b>] if <closed_interval> is true,
@@ -39,19 +68,16 @@ def newton_cotes(p, func, a, b, closed_interval=True):
 
     
 if __name__ == '__main__':
-    
-    prev = 100
-    current = 0
-    tolerance = 0.000001
 
     for i in range(1, 10, 1):
-
         a, b = 0, 1
         
-        print('grau {} aberto = {}'.format(
-            i, newton_cotes(i, lambda x: (sin(2*x) + 4*x**2 + 3*x)**2, a, b, True)
+        print(30 * '-')
+        print('POLINOMIO GRAU ' + str(i))
+        print('aberto:  iteracoes = {}, valor aprox. = {}'.format(
+            *newton_cotes(i, lambda x: (sin(2*x) + 4*x**2 + 3*x)**2, a, b, closed_interval=False)
         ))
-        print('grau {}  fechado  = {}'.format(
-            i, newton_cotes(i, lambda x: (sin(2*x) + 4*x**2 + 3*x)**2, a, b, False)
+        print('fechado: iteracoes = {}, valor aprox. = {}'.format(
+            *newton_cotes(i, lambda x: (sin(2*x) + 4*x**2 + 3*x)**2, a, b)
         ))
         print('')
