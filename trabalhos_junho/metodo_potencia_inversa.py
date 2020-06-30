3d3c3889b26d674c8a6f29d6d50fefad76e82e9d
Natália Rios <36707262+natalia-rios@users.noreply.github.com>
2020-06-29 22:52:42 -0300

    Create metodo_potencia_inversa.py

---
+++ trabalhos_junho/metodo_potencia_inversa.py
@@ -0,0 +1,20 @@
+import scipy
+import numpy as np
+import scipy.linalg as splalg
+
+def metodo_inverso(A, x, epsilon):
+    
+    LUP = splalg.lu_factor(A, overwrite_a = True)
+    n = A.shape[0]
+    splalg.lu_solve(LUP, x, overwrite_b=True )
+    l_old = 0
+    lmin = 1./np.linalg.norm(x)
+    x = x * lmin
+    
+    while(abs(lmin-l_old) > epsilon*lmin):
+        l_old = lmin
+        splalg.lu_solve(LUP, x, overwrite_b=True )
+        lmin = 1./np.linalg.norm(x)
+        x *= lmin
+        
+    return lmin, x
