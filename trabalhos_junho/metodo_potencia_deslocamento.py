a094a28df128d42b0a2aefaeace6b4253580601b
Natália Rios <36707262+natalia-rios@users.noreply.github.com>
2020-06-29 22:53:37 -0300

    Create metodo_potencia_deslocamento.py

---
+++ trabalhos_junho/metodo_potencia_deslocamento.py
@@ -0,0 +1,19 @@
+import scipy
+import numpy as np
+import scipy.linalg as splalg
+
+import metodo_inverso
+
+def metodo_potencia_deslocamento(A, u, x, epsilon):
+  
+    Au = A
+    
+    for i in range (0,len(A)):
+        Au[i][i] = A[i][i] - u
+        
+        
+    inverse = metodo_inverso(Au, x, epsilon) 
+    ans1 = inverse[0] + u
+    ans2 = inverse[1]
+
+    return ans1, ans2
